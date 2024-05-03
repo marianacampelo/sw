07f55631fbb19dbab96b6b46987bbfdbffc64337
Mariana Campello <125597199+marianacampelo@users.noreply.github.com>
2024-05-03 17:45:22 -0300

    Add files via upload

---
+++ Calculadora_SW/Calculadora/code.js
@@ -0,0 +1,39 @@
+function n1(){
+    alert("1")
+}
+
+function n2(){
+    alert("2")
+}
+
+function n3(){
+    alert("3")
+}
+
+function n4(){
+    alert("4")
+}
+
+function n5(){
+    alert("5")
+}
+
+function n6(){
+    alert("6")
+}
+
+function n7(){
+    alert("7")
+}
+
+function n8(){
+    alert("8")
+}
+
+function n9(){
+    alert("9")
+}
+
+function n0(){
+    alert("0")
+}
