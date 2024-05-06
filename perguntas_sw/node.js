c14f2a5201fd4e8452349d43079233954d3b0438
Mariana Campello <125597199+marianacampelo@users.noreply.github.com>
2024-05-06 09:03:13 -0300

    versão atualizada

---
+++ perguntas_sw/node.js
@@ -0,0 +1,15 @@
+function resultado(variavel){
+    if (variavel == 1){
+        alert("Correto")
+    } else{
+        alert("Incorreto")
+    }
+    }
+    
+    function impar(numero){
+        if (numero%2 ==0){
+            alert("PAR")
+        } else{
+            alert("IMPAR")
+        }
+    }
