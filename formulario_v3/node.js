225e30215c38c499ec8b87e352681cca27a53e10
Mariana Campello <125597199+marianacampelo@users.noreply.github.com>
2024-08-02 17:57:20 -0300

    versão final

---
+++ formulario_v3/node.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    alert("Seu site carregou!");
+    
+    $("#btNome").on("click", function(){
+        alert("Tem certeza?");
+    })
+
+    $("#btCPF").on("click", function(){
+        alert("Tem certeza?");
+    })
+
+    $("#btTelefone").on("click", function(){
+        alert("Tem certeza?");
+    })
+
+    $("#btemail").on("click", function(){
+        alert("Tem certeza?");
+    })
+
+    $("#cmbCidade").on("change", function(){
+        alert("Tem certeza?");
+    })
+
+    $("#btCadastro").on("click", function(){
+        alert("Seja Bem-Vindo!!");
+    });
+
+});
